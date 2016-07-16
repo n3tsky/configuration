46a1919274a4acc41c300485cc76626d0c73400b
n3tsky <n3tsky@protonmail.com>
2016-07-16 22:37:39 +0200

    Update ubuntu_hardening.sh

--- ubuntu_hardening.sh
+++ ubuntu_hardening.sh
@@ -5,28 +5,49 @@ sudo sh -c 'printf "[SeatDefaults]\ngreeter-show-remote-login=false\n" >/usr/sha
 sudo sh -c 'printf "[SeatDefaults]\nallow-guest=false\n" >/usr/share/lightdm/lightdm.conf.d/50-no-guest.conf'
 
 echo "[!] Hardening TCP/IP stack"
-echo "net.ipv4.icmp_echo_ignore_broadcasts = 1" >> /etc/sysctl.d/10-network-security.conf
-echo "net.ipv4.conf.all.accept_source_route = 0" >> /etc/sysctl.d/10-network-security.conf
-echo "net.ipv6.conf.all.accept_source_route = 0" >> /etc/sysctl.d/10-network-security.conf
-echo "net.ipv4.conf.default.accept_source_route = 0" >> /etc/sysctl.d/10-network-security.conf
-echo "net.ipv6.conf.default.accept_source_route = 0" >> /etc/sysctl.d/10-network-security.conf
-echo "net.ipv4.conf.all.send_redirects = 0" >> /etc/sysctl.d/10-network-security.conf
-echo "net.ipv4.conf.default.send_redirects = 0" >> /etc/sysctl.d/10-network-security.conf
-echo "net.ipv4.tcp_max_syn_backlog = 2048" >> /etc/sysctl.d/10-network-security.conf
-echo "net.ipv4.tcp_synack_retries = 2" >> /etc/sysctl.d/10-network-security.conf
-echo "net.ipv4.tcp_syn_retries = 5" >> /etc/sysctl.d/10-network-security.conf
-echo "net.ipv4.conf.all.log_martians = 1" >> /etc/sysctl.d/10-network-security.conf
-echo "net.ipv4.icmp_ignore_bogus_error_responses = 1" >> /etc/sysctl.d/10-network-security.conf
-echo "net.ipv4.conf.all.accept_redirects = 0" >> /etc/sysctl.d/10-network-security.conf
-echo "net.ipv6.conf.all.accept_redirects = 0" >> /etc/sysctl.d/10-network-security.conf
-echo "net.ipv4.conf.default.accept_redirects = 0" >> /etc/sysctl.d/10-network-security.conf
-echo "net.ipv6.conf.default.accept_redirects = 0" >> /etc/sysctl.d/10-network-security.conf
-echo "net.ipv4.icmp_echo_ignore_all = 1" >> /etc/sysctl.d/10-network-security.conf
-
-service procps start
+sudo echo "net.ipv4.icmp_echo_ignore_broadcasts = 1" >> /etc/sysctl.conf
+sudo echo "net.ipv4.conf.all.accept_source_route = 0" >> /etc/sysctl.conf
+sudo echo "net.ipv6.conf.all.accept_source_route = 0" >> /etc/sysctl.conf
+sudo echo "net.ipv4.conf.default.accept_source_route = 0" >> /etc/sysctl.conf
+sudo echo "net.ipv6.conf.default.accept_source_route = 0" >> /etc/sysctl.conf
+sudo echo "net.ipv4.conf.all.send_redirects = 0" >> /etc/sysctl.conf
+sudo echo "net.ipv4.conf.default.send_redirects = 0" >> /etc/sysctl.conf
+sudo echo "net.ipv4.tcp_max_syn_backlog = 2048" >> /etc/sysctl.conf
+sudo echo "net.ipv4.tcp_synack_retries = 2" >> /etc/sysctl.conf
+sudo echo "net.ipv4.tcp_syn_retries = 5" >> /etc/sysctl.conf
+sudo echo "net.ipv4.conf.all.log_martians = 1" >> /etc/sysctl.conf
+sudo echo "net.ipv4.icmp_ignore_bogus_error_responses = 1" >> /etc/sysctl.conf
+sudo echo "net.ipv4.conf.all.accept_redirects = 0" >> /etc/sysctl.conf
+sudo echo "net.ipv6.conf.all.accept_redirects = 0" >> /etc/sysctl.conf
+sudo echo "net.ipv4.conf.default.accept_redirects = 0" >> /etc/sysctl.conf
+sudo echo "net.ipv6.conf.default.accept_redirects = 0" >> /etc/sysctl.conf
+sudo echo "net.ipv4.icmp_echo_ignore_all = 1" >> /etc/sysctl.conf
 
 echo "[!] Disable IPv6"
-echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+sudo echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+sudo echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+sudo echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
 sudo sysctl -p
+#cat /proc/sys/net/ipv6/conf/all/disable_ipv6
+
+echo "[!] Remove users"
+for users in games gnats irc list news uucp; do
+  sudo userdel -r "$users" 2> /dev/null
+done
+
+echo "[!] Shared memory"
+sudo echo "tmpfs     /run/shm     tmpfs     defaults,noexec,nosuid     0     0" >> /etc/fstab
+
+echo "[!] DNS"
+sudo echo "nospoof on" >> /etc/host.conf
+
+echo "[!] 'su' and shadow user"
+read -p "   Username for current user: " mycurrentuser
+read -p "   Username for shadow user: " myshadowuser
+sudo useradd -d /home/$myshadowuser -s /bin/bash -m $myshadowuser
+sudo usermod -a -G sudo $myshadowuser
+sudo passwd $myshadowuser
+sudo groupadd admin
+sudo usermod -a -G admin $mycurrentuser
+sudo dpkg-statoverride --update --add root admin 4750 /bin/su
+#sudo passwd -l root
