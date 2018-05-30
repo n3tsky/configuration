f720dfb17a1c3fa8f38b36b17c2f1f12f2f2d6ff
n3tsky <n3tsky@protonmail.com>
2018-05-30 18:10:03 +0200

    updated requirements

--- dotfiles/i3/requirements.sh
+++ dotfiles/i3/requirements.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install dunst lm-sensors i3blocks i3lock i3-wm evince zsh pavucontrol volumeicon-alsa compton scrot feh rofi lxappearance
+sudo apt install dunst lm-sensors i3blocks i3lock i3-wm evince zsh pavucontrol volumeicon-alsa compton scrot feh rofi lxappearance xautolock
